77787f2d90b249b56d6dcdd510e70059f4629dd7
kacijcox <145575906+kacijcox@users.noreply.github.com>
2024-04-17 21:13:11 +0000

    sage shop link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return ( 
             <div className="links">
                 <a href="/">home</a>
-                <a href="https://www.kacicox.com/sage-stop">sage stop</a>
+                <a href="https://sage-stop-nine.vercel.app/">sage stop</a>
                 <a href="./second-project">java rental</a>
                 <a href="./third-project">coin track</a>
             </div>
