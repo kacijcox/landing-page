c77144deb84ee0fa2c37cd8dcbf6c6f199cfdba7
kacijcox <145575906+kacijcox@users.noreply.github.com>
2024-05-15 17:19:21 +0000

    soon

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,8 +3,8 @@ const Navbar = () => {
             <div className="links">
                 <a href="/">home</a>
                 <a href="https://sage-stop-nine.vercel.app/">sage stop</a>
-                <a href="./second-project">java rental</a>
-                <a href="./third-project">coin track</a>
+                <a href="./second-project">java rental (soon)</a>
+                <a href="./third-project">coin track (soon)</a>
             </div>
      );
 }
